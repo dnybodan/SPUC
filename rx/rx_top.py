264c6a516d2c6171feb97802460986e1d7e1552d
Dan Nybo <dnybo.dan@gmail.com>
2023-03-30 23:24:09 -0600

    updated spreading sequence

--- rx/rx_top.py
+++ rx/rx_top.py
@@ -1,24 +1,53 @@
 import iio
-import time
+import numpy as np
 
 tx_dev = iio.Context('ip:192.168.2.1')
 rx_dev = iio.Context('ip:192.168.2.2')
 
-tx_buff = tx_dev.find_device("cf-ad9361-dds-core-lpc").find_channel("sin")
+tx_buff = tx_dev.find_device("cf-ad9361-dds-core-lpc").find_channel("voltage0")
 rx_buff = rx_dev.find_device("cf-ad9361-lpc").find_channel("voltage0")
 
-tx_buff.attrs['frequency'].value = str(2e6)
+tx_buff.attrs['frequency'].value = str(2.4e9)
 tx_buff.attrs['scale'].value = str(1)
 tx_buff.attrs['sampling_frequency'].value = str(10e6)
 
-rx_data = []
+rx_buff.attrs['frequency'].value = str(2.4e9)
+rx_buff.attrs['scale'].value = str(1)
+rx_buff.attrs['sampling_frequency'].value = str(10e6)
 
-for i in range(1024):
-    rx_data.append(int.from_bytes(rx_buff.read(2), byteorder='little', signed=True))
+# Set the spreading sequence
+spreading_seq = np.array([-1, 1, -1, -1, 1, 1, -1, 1, 1, 1, -1, -1, 1, -1, -1, -1,
+                          -1, -1, -1, 1, -1, -1, -1, 1, 1, 1, -1, 1, 1, -1, -1, 1,
+                          1, 1, 1, -1, -1, -1, -1, -1, 1, -1, -1, 1, 1,
+                          -1, 1, -1, -1, 1, 1, -1, 1, 1, 1, -1, -1, 1, -1, -1, -1,
+                          -1, -1, -1, 1, -1, -1, -1, 1, 1, 1, -1, 1, 1, -1, -1, 1,
+                          1, 1, 1, -1, -1, -1, -1, -1, 1, -1, -1, 1, 1])
+
+# Decode the DSSS signal
+rx_data = []
+while True:
+    samples = rx_buff.read(1024)
+    if len(samples) == 0:
+        break
+    for sample in samples:
+        rx_data.append(sample)
+
+msg_dsss = ''
+for i in range(0, len(rx_data), len(spreading_seq)):
+    symbol = rx_data[i:i+len(spreading_seq)]
+    if np.all(symbol == spreading_seq):
+        msg_dsss += '1'
+    elif np.all(symbol == -spreading_seq):
+        msg_dsss += '0'
+
+# Convert the DSSS signal back to text
+msg = ''
+for i in range(0, len(msg_dsss), 8):
+    byte = msg_dsss[i:i+8]
+    msg += chr(int(byte, 2))
+
+print(msg)
 
 rx_buff.dispose()
-tx_buff.dispose()
-rx_dev.dispose()
 tx_dev.dispose()
-
-print(rx_data)
+rx_dev.dispose()
